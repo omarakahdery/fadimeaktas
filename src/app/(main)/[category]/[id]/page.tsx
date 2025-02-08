2cb2eca34201189589cce42d9f7369ae0f9259d5
omarakahdery <85066587+omarakahdery@users.noreply.github.com>
2025-02-08 10:25:45 +0300

    fix: build error 2

--- src/app/(main)/[category]/[id]/page.tsx
+++ src/app/(main)/[category]/[id]/page.tsx
@@ -2,16 +2,20 @@ import { Collections } from "@/containers/collections/list";
 import { Suspense } from "react";
 import { LoadingSpinner } from "@/components/loading-spinner";
 import { CollectionTitle } from "@/containers/collections/title";
+import { use } from "react"
 
-export default async function CategoryPage({
-                                             params,
-                                             searchParams,
-                                           }: {
-  params: { id: string; category: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+export default function CategoryPage({
+                                       params,
+                                       searchParams,
+                                     }: {
+  params: Promise<{ id: string; category: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
-  const { id, category } = params
-  const { orderby, order } = searchParams
+  const resolvedParams = use(params)
+  const resolvedSearchParams = use(searchParams)
+
+  const { id, category } = resolvedParams
+  const { orderby, order } = resolvedSearchParams
 
   return (
     <main className="content-wrapper">
