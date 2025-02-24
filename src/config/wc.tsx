c2ae6a9a9f96202453e282a861920a6868632a1d
omarakahdery <85066587+omarakahdery@users.noreply.github.com>
2025-02-24 18:43:17 +0300

    update: cookieDomain2

--- src/config/wc.tsx
+++ src/config/wc.tsx
@@ -1,6 +1,6 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
-export const cookieDomain = "api.fadimeaktas.com"
+export const cookieDomain = ".fadimeaktas.com"
 export const consumerKey="ck_f333d670c4c587cdc509bf971ef642e49ae00b84"
 export const consumerSecret="cs_300f399490e4138310ea2ca9676b28135657713d"
 export const api = new WooCommerceRestApi({
