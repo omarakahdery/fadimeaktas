7274bb04b0a291f41769b54a8a45dd9e34a79f27
omarakahdery <85066587+omarakahdery@users.noreply.github.com>
2025-02-25 15:27:52 +0300

    update: auth page

--- src/app/(auth)/giris-yap/page.tsx
+++ src/app/(auth)/giris-yap/page.tsx
@@ -1,9 +1,13 @@
 "use server"
 import { Login } from "@/containers/auth/login";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function LoginPage (){
   const cookiesStore = await cookies()
   const cart_key = cookiesStore.get("cart_key")?.value
+
+  redirect("https://api.fadimeaktas.com/hesabim/")
+
   return <Login cart_key={cart_key} />
 }
--- src/app/(auth)/kaydol/page.tsx
+++ src/app/(auth)/kaydol/page.tsx
@@ -1,9 +1,12 @@
 "use server"
 import { cookies } from "next/headers";
 import Signup from "@/containers/auth/singup";
+import { redirect } from "next/navigation";
 
 export default async function SignupPage (){
   const cookiesStore = await cookies()
   const cart_key = cookiesStore.get("cart_key")?.value
+  redirect("https://api.fadimeaktas.com/hesabim/")
+
   return <Signup cart_key={cart_key} />
 }
